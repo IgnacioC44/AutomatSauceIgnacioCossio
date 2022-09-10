f713cca98a8c06e8013f7d50f642f2beff7d4f6e
IgnacioC44 <2788936@gmail.com>
2022-09-10 10:33:36 -0400

    cucumber last changes

--- Automat2/src/test/java/TestRunner.java
+++ Automat2/src/test/java/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/Login.feature",
         glue = {"stepDefinitions"},
-        tags = "@VerifyLowToHighFilter"
+        tags = "@CartIncremented"
 
 )
 public class TestRunner {
